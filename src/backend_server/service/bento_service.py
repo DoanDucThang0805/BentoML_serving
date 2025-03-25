4614eb535db64b113500872a9aa5da91f517fa44
DoanDucThang0805 <ducthang8558@gmail.com>
2025-03-25 13:47:41 +0700

    them xu ly bat dong bo

--- src/backend_server/service/bento_service.py
+++ src/backend_server/service/bento_service.py
@@ -5,9 +5,9 @@ from bentoml.io import NumpyNdarray
 
 BENTO_MODEL_TAG = "timeseries_model:trurguaha67zouhl"
 forcast_runner = bentoml.keras.get(BENTO_MODEL_TAG).to_runner()
-forcast_service = bentoml.Service("forcasting", runners=[forcast_runner])
+forecast_service = bentoml.Service("forecasting", runners=[forcast_runner])
 
 
-@forcast_service.api(input=NumpyNdarray(), output=NumpyNdarray())
-def forecast(input: np.ndarray) -> np.ndarray:
-    return forcast_runner.predict.run(input)
+@forecast_service.api(input=NumpyNdarray(), output=NumpyNdarray())
+async def forecast(input: np.ndarray) -> np.ndarray:
+    return await forcast_runner.predict.async_run(input)
